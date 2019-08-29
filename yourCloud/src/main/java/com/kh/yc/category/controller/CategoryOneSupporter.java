e94c622b81e90e322f1b106c577417d1eed2e417
BGS <BGS@DESKTOP-KOPSML6>
2019-08-29 17:53:10 +0900

    2019 08 29 권석

--- yourCloud/src/main/java/com/kh/yc/category/controller/CategoryOneSupporter.java
+++ yourCloud/src/main/java/com/kh/yc/category/controller/CategoryOneSupporter.java
@@ -6,16 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-@Controller
-public class CategoryOne {
 
-   
-	      @RequestMapping(value = "/categoryOne.ca", method = RequestMethod.GET)
-	      public String home(Locale locale, Model model) {
-	      
-	         
-	         
-	         return "main/categoryOne";
-	      
-	}
+@Controller
+public class CategoryOneSupporter {
+	 @RequestMapping(value = "/categoryOneSupporter.ca", method = RequestMethod.GET)
+     public String home(Locale locale, Model model) {
+     
+        
+        
+        return "main/categoryOneSupporter";
+     
+}
 }
